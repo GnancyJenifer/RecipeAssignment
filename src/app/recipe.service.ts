d3f9f59e71c1071719bfb339d7ae5191add2548a
GnancyJenifer <gnancyjenifer@gmail.com>
2021-06-27 23:59:46 +0530

    RecipeApp

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -10,22 +10,28 @@ import { Recipe } from './models/recipe';
 })
 export class RecipeService {
 
-  recipesUrl = 'http://localhost:8082/';
+  recipesUrl = 'https://localhost:8082/';
   recipes: Recipe[] = [];
   constructor(private httpClient: HttpClient) {
     this.recipes = MockData.Recipes;
   }
   getRecipess():  Observable<Recipe[] >{
-    return this.httpClient.get<Recipe[]>(this.recipesUrl+"recipies");
+    return this.httpClient.get<Recipe[]>(this.recipesUrl+"recipies",
+    { headers: { authorization: this.createBasicAuthToken("user", "pass") } });
+  }
+  createBasicAuthToken(username: String, password: String) {
+    return 'Basic ' + window.btoa(username + ":" + password)
   }
   getRecipe(id: number): Observable<Recipe> {
     let recipe=this.recipes.find( p => p.recipeId === id)
     recipe=recipe!=undefined?recipe:new Recipe();
-    return this.httpClient.get<Recipe>(this.recipesUrl+"recipie/"+id);
+    return this.httpClient.get<Recipe>(this.recipesUrl+"recipie/"+id,
+    { headers: { authorization: this.createBasicAuthToken("user", "pass") } });
   }
   removeRecipe(recipe: Recipe) {
     const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+      headers: new HttpHeaders({ 'Content-Type': 'application/json',
+      authorization: this.createBasicAuthToken("user", "pass") })
     };
     const id = typeof recipe === 'number' ? recipe : recipe.recipeId;
     const url = `${this.recipesUrl}recipie/${id}`;
@@ -36,13 +42,15 @@ export class RecipeService {
   addRecipe(recipe: Recipe) {   
     console.log("add recipe"+recipe);
     const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+      headers: new HttpHeaders({ 'Content-Type': 'application/json',
+      authorization: this.createBasicAuthToken("user", "pass") })
     };
     return this.httpClient.post(this.recipesUrl+"recipie/add", recipe, httpOptions)
   }
   updateRecipe(recipe: Recipe) {  
     const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+      headers: new HttpHeaders({ 'Content-Type': 'application/json',
+      authorization: this.createBasicAuthToken("user", "pass") })
     };
     const url = `${this.recipesUrl}recipie/update/${recipe.recipeId}`;
     return this.httpClient.put(url, recipe, httpOptions)
